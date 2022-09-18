e7898ffa14499eb026c32d68d51239c9a4aa824e
toshtemirov003 <toshtemirovmurodjon03@gmail.com>
2022-09-18 23:24:35 +0500

    Add Tickets section

--- src/pages/Tickets/Tickets.js
+++ src/pages/Tickets/Tickets.js
@@ -1,7 +1,203 @@
-import "./tickets.css"
+import "./tickets.css";
 
 export const Tickets = () => {
   return (
-    <h2>Tickets</h2>
-  )
-}
+    <div className="allTicket-wrapper tickets">
+      <div className="altic-desc">
+        <h2 className="altic-desc__title">All tickets</h2>
+        <div className="text-wrapper">
+          <p className="altic-desc__text altic-desc__text--sort">Sort</p>
+          <p className="altic-desc__text altic-desc__text--filter ">Filter</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <span className="span1">Ticket details</span>
+          <span className="span2">Customer name</span>
+          <span className="span3">Date</span>
+          <span className="span4">Priority</span>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                Contact Email not Linked
+              </strong>
+              <p className="tom-cruise__days">Updated 1 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Tom Cruise</strong>
+            <p className="tom-cruise__date">on 24.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 26, 2019</strong>
+            <p className="date__hour">6:30 PM</p>
+          </div>
+          <p className="priority">High</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--matt ">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                Adding Images to Featured Posts
+              </strong>
+              <p className="tom-cruise__days">Updated 1 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Matt Damon</strong>
+            <p className="tom-cruise__date">on 24.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 26, 2019</strong>
+            <p className="date__hour">8:00 AM</p>
+          </div>
+          <p className="priority priority--low">low</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--robert">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                When will I be charged this month?
+              </strong>
+              <p className="tom-cruise__days">Updated 1 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Robert Downey</strong>
+            <p className="tom-cruise__date">on 24.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 26, 2019</strong>
+            <p className="date__hour">7:30 PM</p>
+          </div>
+          <p className="priority">High</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--christian">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                Payment not going through
+              </strong>
+              <p className="tom-cruise__days">Updated 2 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Christian Bale</strong>
+            <p className="tom-cruise__date">on 24.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 25, 2019</strong>
+            <p className="date__hour">5:00 PM</p>
+          </div>
+          <p className="priority priority--normal">Normal</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--henry">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                Unable to add replies
+              </strong>
+              <p className="tom-cruise__days">Updated 2 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Henry Cavil</strong>
+            <p className="tom-cruise__date">on 24.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 25, 2019</strong>
+            <p className="date__hour">4:00 PM</p>
+          </div>
+          <p className="priority">High</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--chris">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                Downtime since last week
+              </strong>
+              <p className="tom-cruise__days">Updated 3 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Chris Evans</strong>
+            <p className="tom-cruise__date">on 23.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 25, 2019</strong>
+            <p className="date__hour">2:00 PM</p>
+          </div>
+          <p className="priority priority--normal">Normal</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--sam">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">Referral Bonus</strong>
+              <p className="tom-cruise__days">Updated 4 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Sam Smith</strong>
+            <p className="tom-cruise__date">on 22.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 25, 2019</strong>
+            <p className="date__hour">11:30 AM</p>
+          </div>
+          <p className="priority priority--low">Normal</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after">
+          <div className="tom-cruise tom-cruise--steve">
+            <div className="tom-cruise__logo">
+              <strong className="tom-cruise__text">
+                How do I change my password?
+              </strong>
+              <p className="tom-cruise__days">Updated 6 day ago</p>
+            </div>
+          </div>
+          <div className="name">
+            <strong className="tom-cruise__name">Steve Rogers</strong>
+            <p className="tom-cruise__date">on 21.05.2019</p>
+          </div>
+          <div className="date">
+            <strong className="date__date">May 24, 2019</strong>
+            <p className="date__hour">1:00 PM</p>
+          </div>
+          <p className="priority priority--normal">Normal</p>
+          <p>:</p>
+        </div>
+      </div>
+      <div className="peoples">
+        <div className="peoples__after peoples__after--page">
+          <span className="page">Rows per page: 8 </span>
+          <span>1-8 of 1240</span>
+        </div>
+      </div>
+    </div>
+  );
+};
